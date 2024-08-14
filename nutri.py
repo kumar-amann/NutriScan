dc0718a7da6792e6664465a70c44fc777723c26d
Aman Kumar <137410641+kumar-amann@users.noreply.github.com>
2024-08-14 12:31:13 +0530

    Update nutri.py

--- nutri.py
+++ nutri.py
@@ -51,7 +51,7 @@ submit=st.button("Tell me the total calories")
 input_prompt="""
                You are an expert in nutritionist where you need to see the food items from the image
                and calculate the total calories, also provide the details of every food items with calories intake
-               is below format
+               and do not mention that you are an AI can can't check the nutrition value and give response in below format
 
                1. Item 1 - no of calories   
                2. Item 2 - no of calories
