7a23a548da51ae06c0c36032a103db4daa70701a
Aman Kumar <137410641+kumar-amann@users.noreply.github.com>
2025-03-21 23:27:54 +0530

    Updated the use_coloum_width to use_container_width

--- nutri.py
+++ nutri.py
@@ -43,7 +43,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 image=""   
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.image(image, caption="Uploaded Image.",  use_container_width = True)
 
 
 submit=st.button("Tell me the total calories")
